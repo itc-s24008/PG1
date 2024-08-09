bedb4dc3e20df9009adebb8bc86a732dc3f45ba4
s24008 <s24008@std.it-college.ac.jp>
2024-08-09 02:37:12 +0000

    ch8１日目

---
+++ 8-3.py
@@ -0,0 +1,37 @@
+import collections
+
+data = 'すもももももももものうち'
+count_dic = {}
+
+for v in data:
+    if v in count_dic:
+        count_dic[v] += 1
+    else:
+        count_dic[v] = 1
+print(count_dic)
+
+count_dic = collections.defaultdict(int)
+for v in data:
+    count_dic[v] += 1
+print(count_dic)
+
+count_dic = collections.defaultdict(list)
+for v in data:
+    count_dic[v].append(v)
+print(count_dic)
+
+count_dic = {}
+for v in data:
+    count_dic.setdefault(v, []).append(v)
+print(count_dic)
+
+counter = collections.Counter(data)
+print(counter)
+
+print(counter['す'])
+print(counter['ぽ'])
+
+CharCount = collections.namedtuple('CharCount', ['char', 'count'])
+mo = CharCount('も', 8)
+print(mo)
+print(mo.char, mo.count)
